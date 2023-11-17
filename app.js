e21147d43209d28c204f40e431b65c812978ed25
Pallav Pushparaj <pallav_pushparaj@persistent.com>
2023-11-17 22:19:28 +0530

    commit and commit

--- app.js
+++ app.js
@@ -291,18 +291,19 @@ const resList = [
 
 const ResturantCard = (props) => {
     const { resData } = props;
+    const { name, costForTwo, avgRating, cuisines, cloudinaryImageId } = resData?.info;
     
     return (
         <div className="res-card">
             <img className="res-logo"
             alt="Oops"
             src={
-                "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.info.cloudinaryImageId}
+                "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}
                 />
-            <h3>{resData.info.name} </h3>
-            <p>{resData.info.costForTwo}</p>
-            <p>{resData.info.avgRating}</p>
-            <p>{resData.info.cuisines.join(",")}</p>
+            <h3>{name} </h3>
+            <p>{costForTwo}</p>
+            <p>{avgRating}</p>
+            <p>{cuisines.join(",")}</p>
         </div>
     )
 }
