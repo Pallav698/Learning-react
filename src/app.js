29cfbe5cb63388cfc8804f5bdf322acf568e38fe
Pallav Pushparaj <pallav_pushparaj@persistent.com>
2023-12-22 12:27:54 +0530

    finalPSLcommit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -7,6 +7,9 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import ResturantMenu from "./components/RestaurantMenu";
+
+
+const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
     return (
@@ -37,6 +40,10 @@ const appRouter = createBrowserRouter([
             {
                 path: "/restaurants/:resId",
                 element: <ResturantMenu />
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback = {<h1>Loading Your Groceries</h1>}><Grocery/></Suspense>
             }
         ],
         errorElement: <Error/>,
---
+++ src/components/Grocery.js
@@ -0,0 +1,7 @@
+const Grocery = () => {
+    return (
+        <h1>Here you can find Groceries</h1>
+    );
+};
+
+export default Grocery;
